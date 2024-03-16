421b90ea42a5ca889a299967f28c4ffce64e6a47
WiTheR60334 <romir.b@ahduni.edu.in>
2024-03-16 16:29:58 +0530

    updated

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Grid from "../../components/Grid/Grid";
+import Info from "../../components/Info/Info";
 
 function Dashboard() {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <Grid />
+      <Info />
     </div>
   );
 }

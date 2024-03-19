d766ae775548749ad6459b06f084e53a01310938
WiTheR60334 <romir.b@ahduni.edu.in>
2024-03-19 10:38:10 +0530

    updated

--- app/Profile/page.jsx
+++ app/Profile/page.jsx
@@ -2,10 +2,7 @@
 import React, { useState } from "react";
 import styles from "./StudentProfile.module.css";
 import { Popconfirm, message } from "antd";
-import { InputText } from "primereact/inputtext";
-import { Dialog } from "primereact/dialog";
-import { Button } from "primereact/button";
-import Avatar from "react-avatar-edit";
+import img from "./profile.png";
 
 function Profile() {
   const [img, setImg] = useState("");
@@ -13,17 +10,6 @@ function Profile() {
   const [src, setsrc] = useState(false);
   const [profile, setprofile] = useState([]);
   const [pview, setpview] = useState(false);
-  const profileFinal = profile.map((item) => item.pview);
-  const onClose = () => {
-    setpview(null);
-  };
-  const onCrop = (view) => {
-    setpview(view);
-  };
-  const saveCropImage = () => {
-    setprofile([...profile, { pview }]);
-    setImgCrop(false);
-  };
 
   const confirm = () => {
     message.success("Updated your details successfully!!!");
@@ -45,66 +31,8 @@ function Profile() {
               objectFit: "cover",
               border: "4px solid #eae7e7be",
             }}
-            onClick={() => setImgCrop(true)}
-            src={profileFinal.length ? profileFinal : img}
+            src={"profile.png"}
             alt=""
-          />
-          <Dialog
-            visible={imgCrop}
-            header={() => (
-              <p htmlFor="" style={{ fontSize: "25px", fontWeight: "bold" }}>
-                Update Profile
-              </p>
-            )}
-            onHide={() => setImgCrop(false)}
-          >
-            <div className={styles.confirmation_content}>
-              <Avatar
-                width={500}
-                height={400}
-                onCrop={onCrop}
-                onClose={onClose}
-                src={src}
-                shadingColor={"#474649"}
-                backgroundColor={"#474649"}
-                className={styles.avatar_container}
-              />
-              <div className={styles.button_container}>
-                <div className={styles.button_wrapper}>
-                  <Button onClick={saveCropImage}>
-                    <i
-                      className="pi pi-check"
-                      style={{
-                        marginRight: "5px",
-                        display: "flex",
-                        justifyContent: "center",
-                      }}
-                    ></i>
-                    Save
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </Dialog>
-          <InputText
-            type="file"
-            accept="image/"
-            style={{ display: "none" }}
-            onChange={(event) => {
-              const file = event.target.files[0];
-              if (file && file.type.substring(0, 5) === "image") {
-                setImg(file);
-              } else {
-                setImg(null);
-              }
-            }}
-            // style={{
-            //   display: "flex",
-            //   textAlign: "center",
-            //   marginTop: "2rem",
-            //   width: "100%",
-            //   marginLeft: "10rem",
-            // }}
           />
         </div>
         <div className={styles.details}>
